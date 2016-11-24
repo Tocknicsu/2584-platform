8450da0e31ffb1ec0f066b2dfea7931cba6f1899
Tocknicsu <wingemerald@gmail.com>
2016-11-24 16:27:06 +0800

    fininsh

--- backend/server.py
+++ backend/server.py
@@ -9,14 +9,47 @@ import tornado.web
 socket_pool = {}
 job_pool = {}
 job_counter = 0
+lock = 1
 
 
 class ApiHandler(tornado.web.RequestHandler):
 
+    @tornado.gen.coroutine
     def post(self):
-        print(json.dumps({k: self.get_argument(k)
-                          for k in self.request.arguments}))
-        self.write("Hello, world")
+        global lock
+        global socket_pool
+        global job_pool
+        global job_counter
+        data = {k: self.get_argument(k) for k in self.request.arguments}
+        if data['studentId'] not in socket_pool:
+            self.write('FAIL')
+            return
+        while lock == 0:
+            continue
+        lock -= 1
+        now_job_counter = job_counter
+        job_counter += 1
+        lock += 1
+        if data['aiType'] == 'solver':
+            data['aiType'] = 'player'
+        elif data['aiType'] == 'evil':
+            data['aiType'] = 'evil'
+        socket_pool[data['studentId']].send_message(
+            "genmove %s %s jid:%d\r\n" % (
+                data['aiType'],
+                data['serializedTiles'],
+                now_job_counter
+            )
+        )
+        times = 0
+        while times < 1000:
+            if now_job_counter in job_pool:
+                self.write(job_pool[now_job_counter])
+                job_pool.pop(now_job_counter)
+                return
+            yield tornado.gen.sleep(0.001)
+            times += 1
+        self.write("TIMEOUT")
 
 
 class RoomHandler(tornado.web.RequestHandler):
@@ -43,16 +76,15 @@ class TcpConnection(object):
         self.stream.read_until(b'\n', self.on_read_line)
         self.name = ""
         print("new connection", address, stream)
+        self.line_counter = 0
 
     def on_read_line(self, data):
         try:
             data = data.decode()
         except:
             return
-        print(data)
         if len(self.name) == 0:
-            name = data.split()[0]
-            print(name)
+            name = data.split()[1]
             if name not in socket_pool:
                 self.name = name
                 socket_pool[self.name] = self
@@ -60,6 +92,16 @@ class TcpConnection(object):
             else:
                 self.send_message("Register Failed.")
                 self.stream.close()
+                return
+        else:
+            if data.find("jid:") != -1:
+                data = data.split()
+                response = data[0]
+                jobid = int(data[1].split(':')[1])
+                global job_pool
+                job_pool[jobid] = response
+                print(jobid, response)
+        self.stream.read_until(b'\n', self.on_read_line)
 
     def send_message(self, data):
         self.stream.write(data.encode())
